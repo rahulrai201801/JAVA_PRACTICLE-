82456ac608ab7bde7cc9667c8c258043fce391f7
rahulrai201801 <101166301+rahulrai201801@users.noreply.github.com>
2022-06-13 01:13:47 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int num1=23;
+        int num2=34;
+        int num3=233;
+        int num=num1+num2+num3;
+        System.out.println(num);
+    }
+}
