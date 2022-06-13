3b9f508a64ded3fab0be5d3d79c72e9948f9bb36
rahulrai201801 <101166301+rahulrai201801@users.noreply.github.com>
2022-06-13 13:54:07 +0530

    Add files via upload

---
+++ URLConnection.java
@@ -0,0 +1,17 @@
+// Ques 23
+
+import java.io.*;
+import java.net.*;
+public class URLConnection {
+    public static void main(String[] args){
+        try{
+            URL url=new URL("https://www.youtube.com/");
+            URLConnection urlcon=url.openConnection();
+            InputStream stream=urlcon.getInputStream();
+            int i;
+            while((i=stream.read())!=-1){
+                System.out.print((char)i);
+            }
+        }catch(Exception e){System.out.println(e);}
+    }
+}
